77d753f00f419e6806b2e549a3bd6b17d4b49a87
RimonAdel <Rimon.Adel.01@Gmail.com>
2021-03-24 16:23:43 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Test from './test/Test';
+
+function App() {
+  console.log(window.location.origin);
+  return (
+    <div className="App">
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
